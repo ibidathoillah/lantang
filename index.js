a83c87cab9dccc46c040c20df5bc8e75f02966eb
akmal <udinakmal27@gmail.com>
2019-06-30 19:17:14 +0700

    image

--- index.js
+++ index.js
@@ -44,6 +44,81 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 
+async function uploadImage(imageBase64,callback){
+	
+
+// Save base64 image to disk
+try
+{
+	// Decoding base-64 image
+	// Source: http://stackoverflow.com/questions/20267939/nodejs-write-base64-image-file
+	function decodeBase64Image(dataString) 
+	{
+	  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+	  var response = {};
+
+	  if (matches.length !== 3) 
+	  {
+		return new Error('Invalid input string');
+	  }
+
+	  response.type = matches[1];
+	  response.data = new Buffer(matches[2], 'base64');
+
+	  return response;
+	}
+
+	// Regular expression for image type:
+	// This regular image extracts the "jpeg" from "image/jpeg"
+	var imageTypeRegularExpression      = /\/(.*?)$/;      
+
+	// Generate random string
+	var crypto                          = require('crypto');
+	var seed                            = crypto.randomBytes(20);
+	var uniqueSHA1String                = crypto
+										   .createHash('sha1')
+											.update(seed)
+											 .digest('hex');
+
+	var base64Data = imageBase64
+	var imageBuffer                      = decodeBase64Image(base64Data);
+	var userUploadedFeedMessagesLocation = './test/';
+
+	var uniqueRandomImageName            = 'image-' + uniqueSHA1String;
+	// This variable is actually an array which has 5 values,
+	// The [1] value is the real image extension
+	var imageTypeDetected                = imageBuffer
+											.type
+											 .match(imageTypeRegularExpression);
+
+	var userUploadedImagePath            = userUploadedFeedMessagesLocation + 
+										   uniqueRandomImageName +
+										   '.' + 
+										   imageTypeDetected[1];
+
+	// Save decoded binary image to disk
+	try
+	{
+	require('fs').writeFile(userUploadedImagePath, imageBuffer.data,  
+							function() 
+							{
+
+								callback(uniqueRandomImageName);
+							  console.log('DEBUG - feed:message: Saved to disk image attached by user:', userUploadedImagePath);
+							});
+	}
+	catch(error)
+	{
+		console.log('ERROR:', error);
+	}
+
+}
+catch(error)
+{
+	console.log('ERROR:', error);
+}
+
+}
 
 
 /**
@@ -125,6 +200,12 @@ app.post('/v1/comment/add', (req, res) => {
 
 app.post('/v1/post/add', (req, res) => {
 
+
+	
+var data = uploadImage(req.body.image, function(namafile){
+
+	req.body.image = namafile
+
 	var Post = new Model.Post(req.body);
 
 	Post.save((err) => {
@@ -134,6 +215,8 @@ app.post('/v1/post/add', (req, res) => {
 			res.status(201).send({ status: 'success', message: 'New record saved' }); // The request save was fulfilled
 	})
 });
+
+})
 
 app.post('/v1/category/add', (req, res) => {
 
